5d734f89ffabb4dca269d0c9ffe8a266e801e6b2
Krzysiek <krzysztof.plonka64@gmail.com>
2023-01-07 13:49:04 +0100

    Cleaned up implementation details.

--- api/main.py
+++ api/main.py
@@ -1,9 +1,4 @@
-from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from api.dependencies import get_session
-from db import models
-from db.repository import ItemRepository
+from fastapi import FastAPI
 
 app = FastAPI()
 
@@ -11,20 +6,3 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/item/{item_id}", response_model=models.Item)
-async def get_item(item_id: int, session: AsyncSession = Depends(get_session)):
-    try:
-        return await ItemRepository.get(session, item_id)
-    except Exception as exc:
-        raise HTTPException(404, detail=str(exc))
-
-
-@app.post("/item", status_code=201)
-async def create_item(item: models.ItemCreate, session: AsyncSession = Depends(get_session)):
-    try:
-        await ItemRepository.add(session, item)
-        await session.commit()
-    except Exception as exc:
-        raise HTTPException(400, detail=str(exc))
